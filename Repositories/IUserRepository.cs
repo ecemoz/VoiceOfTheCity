05b78c64e8e0bdd6d9799a69f82b69aa033f1b69
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-01 21:26:20 +0300

    IUserRepository is finished

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -2,5 +2,16 @@ using VoiceOfTheCity.Models;
 
 namespace VoiceOfTheCity.Repositories {
 
-    public 
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetByEmailAsync(string email);
+
+        Task<User> GetByUsernameAsync(string username);
+
+        Task<User> AuthenticateAsync(string email, string password);
+
+        Task ChangePasswordAsync(int userId, string newPassword);
+
+        Task<bool>IsEmailExistsAsync(string email);
+    }
 }
